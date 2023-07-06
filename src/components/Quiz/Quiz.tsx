9796a4d5c6b120815a90f8d877b91cc8c63cac01
AlexanderTabakov <sir.alexander.tabakov@gmail.com>
2023-07-06 15:41:33 +0700

    test

---
+++ src/components/Quiz/Quiz.tsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import Question from '../Question';
+import question from "../Question";
+
+const questions = [
+    {
+        question: 'What is the capital of France?',
+        choices: ['Paris', 'London', 'New York'],
+        answer: 'Paris',
+    },
+    {
+        question: 'What is the largest planet in our solar system?',
+        choices: ['Mars', 'Jupiter', 'Venus'],
+        answer: 'Jupiter',
+    },
+    {
+        question: 'What is the boiling point of water?',
+        choices: ['100°C', '0°C', '50°C'],
+        answer: '100°C',
+    },
+    {
+        question: 'What is the largest planet in our solar system?',
+        choices: ['Mars', 'Jupiter', 'Venus'],
+        answer: 'Jupiter',
+    },
+    {
+        question: 'What is the boiling point of water?',
+        choices: ['100°C', '0°C', '50°C'],
+        answer: '100°C',
+    },
+];
+
+const Quiz: React.FC = () => {
+    const [question, setQuestion] = useState(0);
+    // const [score, setScore] = useState(0);
+    //
+    const handleAnswer = (answer: string) => {
+        if (answer === questions[question].answer) {
+            console.log('test');
+        }
+    //
+    //     const nextQuestion = currentQuestion + 1;
+    //     if (nextQuestion < questions.length) {
+    //         setCurrentQuestion(nextQuestion);
+    //     } else {
+    //         alert(`Quiz finished. You scored ${score}/${questions.length}`);
+    //     }
+    // };
+
+    return (
+        <div>
+            {/*<h1 className="text-center">Quiz App</h1>*/}
+            {/*{currentQuestion < questions.length ? (*/}
+            {/*    <Question*/}
+            {/*        question={questions[currentQuestion].question}*/}
+            {/*        choices={questions[currentQuestion].choices}*/}
+            {/*        answer={questions[currentQuestion].answer}*/}
+            {/*        onAnswer={handleAnswer}*/}
+            {/*    />*/}
+            {/*) : "null"*/}
+            {/*}*/}
+            <div>
+                {questions.map(({question, choices, answer}) =>
+                    <Question
+                        question={question}
+                        choices={choices}
+                        answer={answer}
+                    />
+
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Quiz;
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import {App} from "app/App";
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from "react-router-dom";
+import Quiz from "components/Quiz/Quiz";
 
 const root = (
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+    <BrowserRouter>
+        <Quiz/>
+    </BrowserRouter>
 )
 
 ReactDOM.render(
